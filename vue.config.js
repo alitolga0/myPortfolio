8a677ba8e1738bdc5a3018695293ded230a88777
alitolga0 <cakiralitolga@gmail.com>
2024-10-05 15:48:05 +0300

    Update vue.config.js for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/myPortfolyo/'
-})
+  publicPath: '/myPortfolyo/', // GitHub Pages için doğru ayar
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Index Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    // Diğer sayfalar...
+  }
+});
